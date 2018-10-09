b3edd384c7925c594d47dd889d290c3037e141cb
Kristofer Riedman <riedmank@hotmail.com>
2018-10-09 09:33:14 -0700

    added try catch statement

--- about-me-quiz/Program.cs
+++ about-me-quiz/Program.cs
@@ -9,53 +9,62 @@ namespace about_me_quiz
             int correctAnswers = 0;
 
             Console.WriteLine("Welcome to the Kris Riedman Quiz");
-            Console.WriteLine("Question 1: How old am I?");
-            if (AgeChecker(int.Parse(Console.ReadLine())) == 0)
+            try
             {
-                Console.WriteLine("Correct!");
-                correctAnswers++;
-            }
-            else
-                Console.WriteLine("Wrong!");
+                Console.WriteLine("Question 1: How old am I?");
+                if (AgeChecker(int.Parse(Console.ReadLine())) == 0)
+                {
+                    Console.WriteLine("Correct!");
+                    correctAnswers++;
+                }
+                else
+                    Console.WriteLine("Wrong!");
 
-            Console.WriteLine("Question 2: Where was I born?");
-            string answer = LocationChecker(Console.ReadLine().ToLower());
-            if (answer == "Correct!")
+                Console.WriteLine("Question 2: Where was I born?");
+                string answer = LocationChecker(Console.ReadLine().ToLower());
+                if (answer == "Correct!")
+                {
+                    Console.WriteLine("Correct!");
+                    correctAnswers++;
+                }
+                else
+                    Console.WriteLine("Wrong!");
+
+                Console.WriteLine("Question 3: What instrument did I play in high school?");
+                if (InstrumentChecker(Console.ReadLine().ToLower()))
+                {
+                    Console.WriteLine("Correct!");
+                    correctAnswers++;
+                }
+                else
+                    Console.WriteLine("Wrong!");
+
+                Console.WriteLine("Question 4: Do I have a cat named Max?(y/n)");
+                if (CatChecker(Console.ReadLine()))
+                {
+                    Console.WriteLine("Correct!");
+                    correctAnswers++;
+                }
+                else
+                    Console.WriteLine("Wrong!");
+
+                Console.WriteLine("Question 5: Was I in the Air Force?(y/n)");
+                if (JobChecker(Console.ReadLine()))
+                {
+                    Console.WriteLine("Correct!");
+                    correctAnswers++;
+                }
+                else
+                    Console.WriteLine("Wrong!");
+            }
+            catch (Exception e)
             {
-                Console.WriteLine("Correct!");
-                correctAnswers++;
+                Console.WriteLine(e.Message);
             }
-            else
-                Console.WriteLine("Wrong!");
-
-            Console.WriteLine("Question 3: What instrument did I play in high school?");
-            if (InstrumentChecker(Console.ReadLine().ToLower()))
+            finally
             {
-                Console.WriteLine("Correct!");
-                correctAnswers++;
+                Console.WriteLine($"Your score: {correctAnswers} out of 5!!");
             }
-            else
-                Console.WriteLine("Wrong!");
-
-            Console.WriteLine("Question 4: Do I have a cat named Max?(y/n)");
-            if (CatChecker(Console.ReadLine()))
-            {
-                Console.WriteLine("Correct!");
-                correctAnswers++;
-            }
-            else
-                Console.WriteLine("Wrong!");
-
-            Console.WriteLine("Question 5: Was I in the Air Force?(y/n)");
-            if (JobChecker(Console.ReadLine()))
-            {
-                Console.WriteLine("Correct!");
-                correctAnswers++;
-            }
-            else
-                Console.WriteLine("Wrong!");
-
-            Console.WriteLine($"You got {correctAnswers} correct answers!!");
         }
 
         static int AgeChecker(int guess)
