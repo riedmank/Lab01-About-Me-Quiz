9a4bff5ff9e7d7d95a32a34b6c2269073685d0e0
Kristofer Riedman <riedmank@hotmail.com>
2018-10-09 08:38:54 -0700

    initial commit

---
+++ about-me-quiz/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace about_me_quiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
